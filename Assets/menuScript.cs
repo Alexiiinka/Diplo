0634a2650667cdcba68fbb578ae56fbb1885ece6
Alexiiinka <sasenka98@gmail.com>
2023-03-06 20:54:20 +0100

    Animacia soko
    + animacia soka v menu
    + funkcny zvuk v menu

---
+++ Assets/menuScript.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class menuScript : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject AmbientMusic;
+    public void RestartScene()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void ChangeOfVolume(Slider tento)
+    {
+        persistenceScript.volume = tento.value;
+        persistenceScriptVolume.thisAmbientMusic.GetComponent<AudioSource>().volume = tento.value;
+    }
+}
